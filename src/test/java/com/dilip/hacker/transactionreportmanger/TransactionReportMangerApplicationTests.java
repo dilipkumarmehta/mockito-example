3718104765250e4036164e9107ce3526b75dc09c
dilipkumarmehta <dilipkumarmehta02@gmail.com>
2018-07-22 19:15:57 +0530

    initial commit

---
+++ src/test/java/com/dilip/hacker/transactionreportmanger/TransactionReportMangerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dilip.hacker.transactionreportmanger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransactionReportMangerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
